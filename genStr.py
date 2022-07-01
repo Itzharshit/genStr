a4e1d40ffcfdcfddb5c4f9ad21b75e5be4723d09
Pyrogramers <92732265+pyrogramers@users.noreply.github.com>
2022-07-01 19:39:39 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -33,8 +33,7 @@ PHONE_NUMBER_TEXT = (
 async def genStr(bot: Bot, msg: Message):
     chat = msg.chat
     api = 17378766
-    if await is_cancel(msg, api.text):
-        return
+    
     try:
         int(api.text)
     except Exception:
@@ -42,12 +41,11 @@ async def genStr(bot: Bot, msg: Message):
         await msg.reply("`API ID Invalid.`\nPress /start to create again.")
         return
     api_id = api.text
-    await api.delete()
+    
     hash = "64363ed3e29119c36ff4ef28deac8645"
-    if await is_cancel(msg, hash.text):
-        return
+    
     api_hash = hash.text
-    await hash.delete()
+
     try:
         client = Client(":memory:", api_id=api_id, api_hash=api_hash)
     except Exception as e:
