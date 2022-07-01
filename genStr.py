956ed7d96f2f5791a051b47eeaf424aa95cea9bb
Pyrogramers <92732265+pyrogramers@users.noreply.github.com>
2022-07-01 18:02:44 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -32,36 +32,6 @@ PHONE_NUMBER_TEXT = (
 @bot.on_message(filters.private & filters.command("start"))
 async def genStr(bot: Bot, msg: Message):
     chat = msg.chat
-    api = await bot.ask(
-        chat.id, API_TEXT.format(msg.from_user.mention)
-    )
-    if await is_cancel(msg, api.text):
-        return
-    try:
-        int(api.text)
-    except Exception:
-        await api.delete()
-        await msg.reply("`API ID Invalid.`\nPress /start to create again.")
-        return
-    api_id = api.text
-    await api.delete()
-    hash = await bot.ask(chat.id, HASH_TEXT)
-    if await is_cancel(msg, hash.text):
-        return
-    api_hash = hash.text
-    await hash.delete()
-    try:
-        client = Client(":memory:", api_id=api_id, api_hash=api_hash)
-    except Exception as e:
-        await bot.send_message(chat.id ,f"**ERROR:** `{str(e)}`\nPress /start to create again.")
-        return
-    try:
-        await client.connect()
-    except ConnectionError:
-        await client.disconnect()
-        await client.connect()
-    await msg.reply("`Successfully Connected to you Client.`")
-    while True:
         number = await bot.ask(chat.id, PHONE_NUMBER_TEXT)
         if not number.text:
             continue
@@ -69,6 +39,8 @@ async def genStr(bot: Bot, msg: Message):
             await client.disconnect()
             return
         phone = number.text
+        api_id = config.API_ID
+        api_hash = config.API_HASH
         await number.delete()
         confirm = await bot.ask(chat.id, f'`Is "{phone}" correct? (y/n):` \n\ntype: `y` (If Yes)\ntype: `n` (If No)')
         if await is_cancel(msg, confirm.text):
